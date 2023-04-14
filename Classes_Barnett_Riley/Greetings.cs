ebde8479e24d38d29e3a5574cca42059da38dfba
Flare <114496550+MkFlare@users.noreply.github.com>
2023-04-14 09:41:24 -0500

    Add project files.

---
+++ Classes_Barnett_Riley/Greetings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Barnett_Riley
+{
+    class Greetings
+    {
+
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Traveker, welcome!");
+        }
+
+        public void Hello(string name)
+        {
+            Console.WriteLine($"{name}, thank you for joining us today!");
+        }
+
+    }
+}
---
+++ Classes_Barnett_Riley/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Classes_Barnett_Riley
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="name"></param>
+            Greetings greetings = new Greetings();
+            //
+            greetings.Welcome();
+            Console.WriteLine("What is your name squire?");
+            string userName = Console.ReadLine();
+
+            //
+            greetings.Hello("Morgan");
+
+        }
+    }
+}
